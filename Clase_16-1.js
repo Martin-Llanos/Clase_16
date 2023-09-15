842b4ee8c87d8192fad0e432237913469ac747ca
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-15 16:25:25 -0300

    Update Clase_16-1.js

--- Clase_16-1.js
+++ Clase_16-1.js
@@ -1,11 +1,11 @@
 const PATH = "tareas/clase-6/Tarea-1/tarea1_clase6.html";
-const NUMERO_INTEGRANTES = 4;
+const NUMERO_INTEGRANTES = 2;
 
 describe("Testeando tarea 1 clase 6", () => {
   it("Testea el uso correcto de la aplicacion", () => {
     cy.visit(PATH);
 
-    cy.get('input[id="numero-de-familiares"]').type("4");
+    cy.get('input[id="numero-de-familiares"]').type("2");
     cy.get('button[id="aceptar"]').click();
 
     cy.get('button[id="limpiar"]').should("be.visible");
@@ -15,8 +15,8 @@ describe("Testeando tarea 1 clase 6", () => {
       .find("div")
       .should("have.length", NUMERO_INTEGRANTES);
 
-    for (let i = 0; i < 4; i++) {
-      cy.get(`input[id='input-${i + 1}']`).type(`${i + 25}`);
+    for (let i = 0; i < 2; i++) {
+      cy.get(`input[id='input-${i + 1}']`).type(`${i + 23}`);
     }
 
     cy.get('button[id="calcular"]').click();
@@ -24,7 +24,7 @@ describe("Testeando tarea 1 clase 6", () => {
     cy.get('strong[id="resultados"]').should("be.visible");
     cy.get('strong[id="resultados"]').should(
       "have.text",
-      "La mayor edad es 28, la menor edad es 25, y el promedio de edades es 26.5"
+      "La mayor edad es 26, la menor edad es 23, y el promedio de edades es 21.5"
     );
   });
 });
