5614749bb3a7504ad5e87cd010df485e07601eda
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-16 16:24:46 -0300

    Update Clase_16-2.js

--- Clase_16-2.js
+++ Clase_16-2.js
@@ -1,6 +1,13 @@
 const PATH = "tareas/clase-6/Tarea-2/tarea2_clase6.html";
 const NUMERO_TRABAJADORES = 5;
 
+const TRABAJADOR_1 = $45000;
+const TRABAJADOR_2 = $40000;
+const TRABAJADOR_3 = $35000;
+const TRABAJADOR_4 = $30000;
+const TRABAJADOR_5 = $25000;
+
+
 describe("testeando tarea 2 de clase 6", () => {
   it("testea el uso correcto de la aplicación", () => {
     cy.visit(PATH);
@@ -24,7 +31,7 @@ describe("testeando tarea 2 de clase 6", () => {
     cy.get('button[id="calcular"]').click();
     cy.get('em[id="resultados"]').should(
       "have.text",
-      "El mayor salario es $50000 el menor salario es $25000 el salario anual promedio es $20000 el salario mensual promedio es $15000"
+      "El mayor salario es $45000 el menor salario es $25000 el salario anual promedio es $20000 el salario mensual promedio es $35000"
     );
     cy.get('button[id="limpiar"]').should("be.visible");
   });
